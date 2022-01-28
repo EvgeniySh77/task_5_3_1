ea13e34169e253cd4eaa17072a7ec9d26077fa12
EvgeniySh77 <evgen.sh77@mail.ru>
2022-01-28 21:25:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task_5_3_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int age;
+            Console.Write("Введите свой возраст: ");
+            age = int.Parse(Console.ReadLine());
+
+            ChangeAge(age);
+            Console.WriteLine(age);
+
+            Console.ReadKey();
+        }
+        
+        static int ChangeAge(int age)
+        {
+            age = 27;
+            return age;
+        }
+    }
+}
